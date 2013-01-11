8f1704f9e4086141fcec76a3c4764ddb933bde02
Chris Harrington <chrisharrington99@gmail.com>
2013-01-11 00:25:10 -0700

    Added the basis of keyboard shortcuts.

--- LogWhatever.MvcApplication/Scripts/Prereqs/Init.js
+++ LogWhatever.MvcApplication/Scripts/Prereqs/Init.js
@@ -12,6 +12,10 @@ LogWhatever.Init = function () {
 
 	    $("button.logger").click(function() { LogWhatever.Logger.show(); });
 	    $("div.tile.selectable, div.tile.deletable").live("click", function () { $(this).toggleClass("selected"); });
+	    $(document).keyup(function (e) {
+	    	if (String.fromCharCode(e.keyCode) == 'L') LogWhatever.Logger.show();
+	    	if (e.keyCode == 27) LogWhatever.Logger.hide();
+	    });
 	    
 	    if (window.location.hash == "")
 		    Finch.navigate("/dashboard");
